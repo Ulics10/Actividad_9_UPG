c480624ebd3ff1a77fdcfe8f63811b667d29d1dd
Ulises101199 <Ulisespg119@gmail.com>
2021-05-28 16:01:54 -0500

    first commit

---
+++ Actividad9_UPG/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Actividad9_UPG
+//
+//  Created by user182915 on 4/13/21.
+//  Copyright © 2021 user182915. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
